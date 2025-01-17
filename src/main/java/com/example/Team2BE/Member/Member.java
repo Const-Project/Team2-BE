cf7524c4aa1445319bf9c246a55e5cddba9a155a
c5ln <cjstjdgh0209@gmail.com>
2025-01-18 00:35:00 +0900

    Member 엔티티 Column 한 개 추가

--- src/main/java/com/example/Team2BE/Member/Member.java
+++ src/main/java/com/example/Team2BE/Member/Member.java
@@ -1,5 +1,16 @@
 package com.example.Team2BE.Member;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Member {
 
+    @Id
+    private Long id;
+
+    @Column
+    private String memberId;
 }

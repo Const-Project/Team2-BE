f662fd80fd1004522675df52d09b560e56b79fd9
c5ln <cjstjdgh0209@gmail.com>
2025-01-21 22:14:51 +0900

    fix : 예악어(Order) 사용 문제 해결, 사용하지않는 메소드 주석 처리

--- src/main/java/com/example/Team2BE/Admin/service/AdminService.java
+++ src/main/java/com/example/Team2BE/Admin/service/AdminService.java
@@ -6,7 +6,6 @@ import com.example.Team2BE.Member.domain.Member;
 import com.example.Team2BE.Member.domain.repository.MemberRepository;
 import com.example.Team2BE.Order.domain.Order;
 import com.example.Team2BE.Order.domain.repository.OrderRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
